35dbb1e28e692584c257fc1fd4d41d25c2da0962
Grace <grace.anderson.au@gmail.com>
2022-05-11 18:11:21 +1000

    created db tables

--- db/schema.sql
+++ db/schema.sql
@@ -12,6 +12,7 @@ CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL,
+  department_id INT,
   FOREIGN KEY (department_id)
     REFERENCES department(id)
     ON DELETE SET NULL
@@ -19,11 +20,13 @@ CREATE TABLE role (
 
 CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(255),
+  first_name VARCHAR(255) NOT NULL,
   last_name VARCHAR(255) NOT NULL,
+  role_id INT,
   FOREIGN KEY (role_id)
     REFERENCES role(id)
     ON DELETE SET NULL,
+  manager_id INT,
   FOREIGN KEY (manager_id)
     REFERENCES employee(id)
     ON DELETE SET NULL
